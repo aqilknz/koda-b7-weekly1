6d34cb3cfc97a7a8949218df75da4273322501bc
aqilknz <154092951+aqilknz@users.noreply.github.com>
2026-02-28 05:27:51 +0700

    Add files via upload

---
+++ task-4.js
@@ -0,0 +1,14 @@
+
+// penggunaan pop()
+const hewan = ["kucing", "babi", "singa", "jerapah"]
+hewan.pop()
+console.log(hewan)
+
+// penggunaan toUpperCase
+const strBesar = "baTu";
+console.log(strBesar.toUpperCase())
+
+// penggunaan slice
+const hewanMamalia = ["kambing", "kucing", "sapi", "kerbau", "domba"];
+const methodSlice = hewanMamalia.slice(1, 3);
+console.log(methodSlice)
