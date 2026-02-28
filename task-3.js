2945c21a221a4a322a91663f48dade454a91e25f
aqilknz <154092951+aqilknz@users.noreply.github.com>
2026-02-28 11:51:23 +0700

    Update task-3.js

--- task-3.js
+++ task-3.js
@@ -1,13 +1,22 @@
 // celcius ke fahrenheit
 function cToF (c) {
+    if (typeof c !== "number" || isNaN(c)){
+        return "harus berupa angka!"
+    }
     return (c * 9/5 + 32);
 }
 // fahrenheit ke celcius
 function fToC (f) {
+    if (typeof f !== "number" || isNaN(f)){
+        return "harus berupa angka!"
+    }
     return (f-32) * 5/9;;
 }
 
-let suhuC = 20
+let suhuC = "nami"
 let suhuF = 75
-console.log(`Suhu : ${suhuC} *C = ${cToF(suhuC)} *F `)
-console.log(`Suhu : ${suhuF} *C = ${fToC(suhuF)} *C `)
+console.log(`Suhu: ${cToF(suhuC)}`)
+console.log(`Suhu: ${fToC(suhuF)}`)
+// console.log(`Suhu : ${suhuF}`)
+// console.log(`Suhu : 30 °C = ${cToF(suhuC)}`);
+// console.log(`$cToF(30), fToC(86)`);
