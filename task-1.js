4686a19e5fd7ed5940fd5aca015b8b6035e01860
aqilknz <154092951+aqilknz@users.noreply.github.com>
2026-02-28 11:16:56 +0700

    Update task-1.js

--- task-1.js
+++ task-1.js
@@ -1,15 +1,19 @@
-function cekPalinfrom(word) {
-    let result = '';
-    for (let i = word.length - 1; i >= 0; i--) {
-        result += word[i];
+function cekPalindrom(word) {
+    const ubahKecil = word.toLowerCase();
+    // console.log(ubahKecil)
+    let result = ""
+    for (let i = ubahKecil.length - 1; i >= 0; i--) {
+        result += ubahKecil[i];
     }
-
-    if (result === word) {
-        return 'Palindrom';
-    } else {
-        return 'Bukan Palindrom';
-    }
+    return result === ubahKecil ? "Palindorm" : "Bukan Palindrom"
+    // if (result === ubahKecil) {
+    //     return 'Palindrom';
+    // } else {
+    //     return 'Bukan Palindrom';
+    // }
 }
 
-const cekWord = 'apa';
-console.log(cekPalinfrom(cekWord));
+const cekWord = 'KAtaK';
+const cekWord2 = 'KAtaK';
+console.log(cekPalindrom(cekWord));
+console.log(cekPalindrom(cekWord2));
