24266c11811eec4b5b8d658c561150fb4c99de7c
aqilknz <154092951+aqilknz@users.noreply.github.com>
2026-03-01 20:43:13 +0700

    Update task-5.js

--- task-5.js
+++ task-5.js
@@ -1,8 +1,23 @@
-function showNumber (n) {
-    for (let i = 1; i <= n; i++) {
-        setTimeout(() => {
-            console.log(i)
-        }, i * 1000)
-    }
-}
-showNumber(5);
+// function showNumber (n) {
+//     for (let i = 1; i <= n; i++) {
+//         setTimeout(() => {
+//             console.log(i)
+//         }, i * 1000)
+//     }
+// }
+// showNumber(5);
+
+
+function showNumber(n) {
+    let i = 1;
+    const interval = setInterval (() =>{
+        console.log(i)
+        if (i >= n) {
+            clearInterval(interval)
+            console.log("selesai")
+        }
+        i++
+    }, 1000
+)}
+
+showNumber(5)
